938a617404c5b0f9644a6927a05034651a7e55bf
math1761 <mathieulevi@gmail.com>
2018-04-27 16:04:34 +0200

    fix errors

--- src/pages/hangman/hangman.ts
+++ src/pages/hangman/hangman.ts
@@ -16,9 +16,9 @@ import {DICTIONARY} from '../../providers/globals';
 })
 export class HangmanPage {
 
-    step =  '';
-    count =  0;
-    word =  "";
+    step = '';
+    count = 0;
+    word = "";
     try = [];
     letters = [];
     err = 0;
@@ -32,30 +32,31 @@ export class HangmanPage {
         const randomIndex = Math.round(Math.random() * DICTIONARY.length);
         this.word = DICTIONARY[randomIndex].key;
     }
+
     // griser valider +11
     public addStep(): void {
         this.step = this.step.toLowerCase();
         if (this.step.match(/[a-zA-Z]/) && this.err < 11) {
-           for (let i = 0; i < this.word.length; i++) {
-               if (this.step == this.word[i]) {
-                 this.goodPass = true;
-                   if (this.word[i]!==this.try[i]){
-                       this.try[i]=this.step;
-                   }
-               } else if (this.word[i]!=this.try[i]) {
-                 this.try[i]='-';
-               }
-           }
-           if(!this.goodPass){
-             this.letters.indexOf(this.step) ? this.err++ : this.err;
-             this.letters.indexOf(this.step) ? this.letters.push(this.step) : null;
-           } else {
-             this.goodPass = false;
-           }
-           this.letters.indexOf(this.step) ? this.count++ : this.count;
-           console.log(this.try);
-           console.log(this.letters);
-        } else if (this.err === 11){
+            for (let i = 0; i < this.word.length; i++) {
+                if (this.step == this.word[i]) {
+                    this.goodPass = true;
+                    if (this.word[i] !== this.try[i]) {
+                        this.try[i] = this.step;
+                    }
+                } else if (this.word[i] != this.try[i]) {
+                    this.try[i] = '-';
+                }
+            }
+            if (!this.goodPass) {
+                this.letters.indexOf(this.step) ? this.err++ : null;
+                this.letters.indexOf(this.step) ? this.letters.push(this.step) : null;
+            } else {
+                this.goodPass = false;
+            }
+            this.letters.indexOf(this.step) ? this.count++ : this.count;
+            console.log(this.try);
+            console.log(this.letters);
+        } else if (this.err === 11) {
             console.log("fini !");
             console.log(this.word);
         }
